6e46865718af30eba0ad5afa65af45ee87909e82
JafarbekZokirov <jafarbekzokirovh@gmail.com>
2024-05-01 13:34:13 +0500

    2-bolim

---
+++ bolim2.js
@@ -0,0 +1,71 @@
+/*-------- Ogohlantirish (alert) --------*/ 
+// alert("Muhim xabar!")
+
+/*-------- So'rov (prompt) --------*/
+// prompt("Ismingizni kiriting", "Jafarbek")
+
+/*-------- Tasdiqlash (confirm) --------*/
+// confirm("Yoshingiz 18 dan oshganmi?")
+
+/*-------- Obyekt --------*/
+// talaba = {ism: "Jasur"}
+// console.log(talaba["ism"])
+
+// console.log("Jafarbek\u0020Zokirov")  
+// console.log("Jafarbek\u0009Zokirov")
+// console.log("Jafarbek\u000BZokirov")
+// console.log("Jafarbek\u000CZokirov")
+// console.log("Jafarbek\u00A0Zokirov")
+// console.log("Jafarbek\u200FZokirov")
+// console.log("Jafarbek\u200EZokirov")
+// console.log("Jafarbek\uFEFFZokirov")
+
+
+/*-------- O'zgaruvchi va o'zgarmaslar --------*/
+// var x = 12;
+// console.log(x); // 12
+// {
+//     var x = 2;
+//     console.log(x); // 2
+// }
+// console.log(x); // 2
+
+// let x = 12;
+// console.log(x); // 12
+// {
+//     let x = 2;
+//     console.log(x); // 2
+// }
+// console.log(x); // 12
+
+/* ----------- FUNCTION ----------- */
+// function x2(a) {
+//     var b = a * 2;
+
+//     return b;
+// }
+
+// console.log(x2(2));
+
+
+/*-------------- Amallar ---------------*/
+// var x = 5; 
+// var y = 2;
+
+// var x = 5, y = 2;
+
+var [x, y] = [5, 2];
+console.log(x, y);
+
+
+
+
+
+
+
+
+
+
+
+
+
